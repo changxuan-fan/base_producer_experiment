6e256f000ff168807878d8acc2ce5519bdc350ff
Changxuan Fan <fanchx@bu.edu>
2024-01-10 08:12:29 -0500

    Debug two players mode

--- client/src/examples/Advertise.jsx
+++ client/src/examples/Advertise.jsx
@@ -54,37 +54,8 @@ import {
       console.log("Saved amountOfWarrant to player.round object: ", amountOfWarrant);
     }
   
-    const isResultStage = stage.get("name") === "result";
   
-    if (players.length > 1) {
-      product = (
-        <div className="grid grid-cols-2 items-center">
-          {product}
-          <div>
-            {isResultStage ? (
-              <>
-                <div className="text-gray-500 text-2xl">You</div>
-                <div className="border-b-3 border-blue-500/50 pb-2 mb-8">
-                  {PlayerScore(player, () => {}, isResultStage)}
-                </div>
-              </>
-            ) : null}
-            {players
-              .filter((p) => p.id !== player.id)
-              .map((p) => PlayerScore(p, handleChange, isResultStage))}
-          </div>
-        </div>
-      );
-    } else if (players.length == 1 && isResultStage) {
-      product = (
-        <div className="grid grid-cols-2 items-center">
-          {product}
-          <div>
-            {isResultStage ? PlayerScore(player, () => {}, isResultStage) : null}
-          </div>
-        </div>
-      );
-    }
+
     return (
       <div className="md:min-w-96 lg:min-w-128 xl:min-w-192 flex flex-col items-center space-y-10">
         {}
@@ -130,9 +101,8 @@ import {
 
           <p>Your current choice of warrant is <b>$ {player.round.get("amountOfWarrant") || 0} </b></p>
           <div className="flex justify-center space-x-4"> 
-              <WarrantButton text={'$50'} on_button_click={(e) => handleWarrantChoice(e, 50)}></WarrantButton>
-   
-              <WarrantButton text={'$300'} on_button_click={(e) => handleWarrantChoice(e, 300)}></WarrantButton>
+              <WarrantButton text={'$20'} on_button_click={(e) => handleWarrantChoice(e, 20)}></WarrantButton>
+              <WarrantButton text={'$100'} on_button_click={(e) => handleWarrantChoice(e, 100)}></WarrantButton>
           </div>
 
 
@@ -218,28 +188,7 @@ import {
     )
   }
 
-  function PlayerScore(player, onChange, isResultStage) {
-    return (
-      <div key={player.id} className="py-4">
-        <div className="flex items-center space-x-6">
-          <div className="h-12 w-12 shrink-0">
-            <Avatar player={player} />
-          </div>
 
-          {isResultStage ? (
-            <div className="flex flex-col items-center space-y-0.5">
-              <div className="text-2xl font-semibold leading-none font-mono">
-                {player.round.get("score") || 0}
-              </div>
-              <h1 className="text-xs font-semibold uppercase tracking-wider leading-none text-gray-400">
-                Score
-              </h1>
-            </div>
-          ) : null}
-        </div>
-      </div>
-    );
-  }
 
 
   function ProfitMarginCalculation({producerPlayer}){
